e08870200754233ba37b8109657e8de206069303
﻿Michelli Melo <241793096+michellimelo@users.noreply.github.com>
2025-12-01 18:51:33 +0100

    reto 3

--- criptografia/reto-3.py
+++ criptografia/reto-3.py
@@ -6,15 +6,15 @@ cipher_b64 = "pl1yjRNniIvptiQIrX7HPeo4w19FLCnBd/WC1UZEHhOdZHe8KTvjuuoXUPlio5q631
 ciphertext = b64decode(cipher_b64)
 
 # Clave (hex) del enunciado
-key_hex = "FF9DF30474898787A45605CCB9B936D33B780D03CABC81719D52383480DC3120"
-key = bytes.fromhex(key_hex)
+clave_hex = "FF9DF30474898787A45605CCB9B936D33B780D03CABC81719D52383480DC3120"
+clave = bytes.fromhex(key_hex)
 
 # Nonce (en base64) del enunciado
 nonce_b64 = "9Yccn/f5nJJhAt2S"
 nonce = b64decode(nonce_b64)
 
 # ChaCha20 "puro": no hay tag ni decrypt_and_verify
-cipher = ChaCha20.new(key=key, nonce=nonce)
+cipher = ChaCha20.new(clave=clave, nonce=nonce)
 plaintext = cipher.decrypt(ciphertext)
 
 print(plaintext.decode("utf-8"))
