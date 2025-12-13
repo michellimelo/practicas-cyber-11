cfb3d7b6de25a888ca4487652bab744c62430281
﻿Michelli Melo <241793096+michellimelo@users.noreply.github.com>
2025-12-13 10:45:16 +0100

    Actualiza prácticas de criptografía

--- criptografia/practica-ejercicio-15.py
+++ criptografia/practica-ejercicio-15.py
@@ -1,4 +1,6 @@
 import re
+from psec import tr31
+
 
 # 1 Bloque TR-31 y KBPK
 
@@ -10,7 +12,7 @@ tr31_hex = re.sub(r"\s+", "", tr31_block).upper()
 
 #-----------------------------------------------------------------------------------------
 
-# 2) Cabecera fija (16 caracteres)
+# 2 Cabecera fija (16 caracteres)
 
 header = tr31_hex[:16]
 
@@ -30,3 +32,15 @@ print("MODE   :", mode)
 print("KVER   :", kver)
 print("EXPORT :", export)
 print("--------------------------------------------------")
+
+# 3 TR-31 UNWRAP 
+
+from psec import tr31
+
+kbpk = bytes.fromhex("A1A10101010101010101010101010102")
+
+key_block = ("D0144D0AB00S000042766B9265B2DF93AE6E29B58135B77A2F616C8D515ACDBE6A5626F79FA7B4071E9EE1423C6D7970FA2B965D18B23922B5B2E5657495E03CD857FD37018E111B")
+
+header, clave = tr31.unwrap(kbpk, key_block)
+
+print("Valor clave:", clave.hex().upper())
